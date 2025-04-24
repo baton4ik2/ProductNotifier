5034a9eca052d94010048018a85981805f817e90
Bulat <akbirov.bulat@yandex.ru>
2025-04-24 12:41:16 +0300

    commit

--- kafka-transactions/TransferService/src/main/java/ru/akbirov/transferservice/service/TransferService.java
+++
@@ -1,7 +0,0 @@
-package ru.akbirov.transferservice.service;
-
-import ru.akbirov.transferservice.model.TransferRestModel;
-
-public interface TransferService {
-    public boolean transfer(TransferRestModel productPaymentRestModel);
-}
